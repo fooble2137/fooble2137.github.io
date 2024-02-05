6fa3c20b363075eab80a14710e1d1e8d4185d5c7
BuildTools <unconfigured@null.spigotmc.org>
2024-02-05 12:14:17 +0100

    added email

--- js/redirect.js
+++ js/redirect.js
@@ -2,10 +2,6 @@ const urlParams = new URLSearchParams(window.location.search);
 const destination = urlParams.get("url");
 
 switch (destination) {
-  case "discord": {
-    window.location.replace("https://discord.com/users/1100806556574044260/");
-    break;
-  }
   case "github": {
     window.location.replace("https://github.com/fooble2137/");
     break;
